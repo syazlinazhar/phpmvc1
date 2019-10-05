71346962c6428c5b7269e4a6b4d9dbbea953b7b7
unknown <amierasyazlin97@gmail.com>
2019-10-06 02:22:11 +0700

    parsing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,14 @@
 class App{
 	public function __construct()
 	{
-		var_dump($_GET);
+		$url = $this->parseURL();
+		var_dump($url);
+	}
+
+	public function parseURL()
+	{
+		if( isset($_GET['url']) ) {
+			$url = $_GET['url'];
+			return $url;
 	}
 }
