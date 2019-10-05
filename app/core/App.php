8a7f99eb7c78f54a92d03aaec9f811118fbd8811
unknown <amierasyazlin97@gmail.com>
2019-10-06 04:12:16 +0700

    modified file App.php (made mistake at line 16)

--- app/core/App.php
+++ app/core/App.php
@@ -13,22 +13,23 @@ class App{
 		if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
 			$this->controller = $url[0];
 			unset($url[0]);
-			var_dump($url);
 	}
 
 	require_once '../app/controllers/' . $this->controller . '.php';
 
 		// method
-	if( isset($url[1]) ){
+		if( isset($url[1]) ){
 			if( method_exists($this->controller, $url[1]) ) {
 				$this->method = $url[1];
 				unset($url[1]);
 			}
-	}
+		}
+
 	//params
 		if( !empty($url) ) {
 			$this->params = array_values($url);
 		}
+
 		// run controller & method, send parameter (if any) 
 		call_user_func_array([$this->controller, $this->method], $this->params);
 }
