c85eac22e42a34146719c71f012509d5224c6dd1
unknown <amierasyazlin97@gmail.com>
2019-10-06 04:09:00 +0700

    choose view to display

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,6 +1,10 @@
 <?php
 
 class About {
+	public function index($name = 'Alin', $as = 'Student')
+	{
+		echo "Hello, nama saya $name, saya adalah seorang $as" ;
+	}
 	public function page()
 	{
 		echo 'About/page';
