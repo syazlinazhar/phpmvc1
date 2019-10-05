a71cb79457c5d3064e83cedf5a6c4666046dbb2e
unknown <amierasyazlin97@gmail.com>
2019-10-06 03:43:59 +0700

    call controller

--- app/core/App.php
+++ app/core/App.php
@@ -9,11 +9,14 @@ class App{
 	{
 		$url = $this->parseURL();
 		
+		//controller
 		if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
 			$this->controller = $url[0];
 			unset($url[0]);
 			var_dump($url);
 	}
+
+	require_once '../app/controllers/' . $this->controller . '.php';
 }
 
 	public function parseURL()
