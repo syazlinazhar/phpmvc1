86d0502b3042a0b510e3c865c17a188438ca32bd
unknown <amierasyazlin97@gmail.com>
2019-10-06 20:07:36 +0700

    call title Home

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,7 +3,8 @@
 class Home extends Controller {
 	public function index()
 	{
-		$this->view('templates/header');
+		$data['title'] = 'Home';
+		$this->view('templates/header', $data);
 		$this->view('home/index');
 		$this->view('templates/footer');
 	}
