ec50ea07053925b87a306d099aed9c8135e0d343
unknown <amierasyazlin97@gmail.com>
2019-10-07 00:58:45 +0700

    create space

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,7 @@
+<div class="container">
 
 	<h1>About Me</h1>
+	<img src="<?= BASEURL; ?>/img/family.jpeg" alt="PRIORITY" width="230" class="rounded-circle shadow">
 	<p>Hello, my name is <?= $data['name']; ?> , I am a <?= $data['as']; ?>, I am <?= $data['age']; ?> years old </p>
 
+</div>
