ddae1edb07137e617855b5471d67010524c7e9eb
unknown <amierasyazlin97@gmail.com>
2019-10-06 19:26:54 +0700

    call controller page

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,12 @@
 <?php
 
 class About extends Controller {
-	
+
 	public function index($name = 'Alin', $as = 'Student', $age = 22)
 	{
 		$this->view('about/index');
 	}
+	
 	public function page()
 	{
 		$this->view('about/page');
