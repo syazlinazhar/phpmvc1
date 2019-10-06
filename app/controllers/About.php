38f081ece095bf38e993da562e28d5ac5433cc53
unknown <amierasyazlin97@gmail.com>
2019-10-06 20:05:33 +0700

    call title About & Page

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -7,11 +7,18 @@ class About extends Controller {
 		$data['name'] = $name;
 		$data['as'] = $as;
 		$data['age'] = $age;
+		$data['title'] = 'About';
+		$this->view('templates/header', $data);
 		$this->view('about/index', $data);
+		$this->view('templates/footer');
+		
 	}
 
 	public function page()
 	{
+		$data['title'] = '';
+		$this->view('templates/header', $data);
 		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
