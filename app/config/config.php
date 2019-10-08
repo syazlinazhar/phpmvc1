48b30722ca57577ad8fb760197bd293cab0065ab
unknown <amierasyazlin97@gmail.com>
2019-10-08 21:07:07 +0700

    config database

--- app/config/config.php
+++ app/config/config.php
@@ -2,3 +2,10 @@
 
 
 define('BASEURL', 'http://localhost/phpmvc1/public');
+
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc1');
