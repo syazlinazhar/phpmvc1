e196b199eedb3d5abc1ce77d4570cef83e67b5c0
unknown <amierasyazlin97@gmail.com>
2019-10-08 18:00:03 +0700

    send data Mahasiswa to views

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -1,11 +1,12 @@
 <?php 
 
-class Mahasiswa{
+class Mahasiswa extends Controller{
 	public function index()
 	{
 		$data['title'] = 'Register Mahasiswa'; 
+		$data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
 		$this->view('templates/header', $data); 
-		$this->view('mahasiswa/index');
+		$this->view('mahasiswa/index', $data);
 		$this->view('templates/footer');
 	} 
 }
