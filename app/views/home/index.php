37880f95b3e9fc7ba3b7ff6d993b5764ae71a95a
unknown <amierasyazlin97@gmail.com>
2019-10-08 16:22:16 +0700

    display model data on views

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,7 +2,7 @@
 	
 	<div class="jumbotron">
 	  <h1 class="display-4">Welcome to My Website!</h1>
-	  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+	  <p class="lead">Hello, my name is <?= $data['name']; ?></p>
 	  <hr class="my-4">
 	  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
 	  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
