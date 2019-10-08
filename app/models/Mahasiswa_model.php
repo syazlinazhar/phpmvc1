90c239494a042a7fa3590e94d552c8855fe9e8f8
unknown <amierasyazlin97@gmail.com>
2019-10-08 18:24:29 +0700

    connect database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,29 +1,49 @@
 <?php 
 
 class Mahasiswa_model {
-	private $mhs = [
-		[
-			"name" => "Amiera Syazlin Binti Md Azhar",
-			"matric no." => "193043920",
-			"email" => "amierasyazlin97@gmail.com",
-			"course" => "Information Technology"
-		],
-		[
-			"name" => "Nur Syaqinah Binti Safaruddin",
-			"matric no." => "193043405",
-			"email" => "syaqinahsafar97@gmail.com",
-			"course" => "Information Technology"
-		],
-		[
-			"name" => "Maizatul Fatihah Binti Mohd Salleh",
-			"matric no." => "193042605",
-			"email" => "maizatulfatihah@ymail.com",
-			"course" => "Information Technology"
-		]
-	];
+	private $dbh; // database handler
+	private $stmt; // private statement to keep query 
+
+	public function __construct()
+	{
+		// data source name
+		$dsn = 'mysql:host=localhost;dbname=phpmvc1';
+		
+		try {
+			$this->dbh = new PDO($dsn, 'root', '');
+		} catch(PDOException $e) {
+			die($e->getMessahe());
+		}
+
+	}
+
+
+	// data using array
+	// private $mhs = [
+	// 	[
+	// 		"name" => "Amiera Syazlin Binti Md Azhar",
+	// 		"matric no." => "193043920",
+	// 		"email" => "amierasyazlin97@gmail.com",
+	// 		"course" => "Information Technology"
+	// 	],
+	// 	[
+	// 		"name" => "Nur Syaqinah Binti Safaruddin",
+	// 		"matric no." => "193043405",
+	// 		"email" => "syaqinahsafar97@gmail.com",
+	// 		"course" => "Information Technology"
+	// 	],
+	// 	[
+	// 		"name" => "Maizatul Fatihah Binti Mohd Salleh",
+	// 		"matric no." => "193042605",
+	// 		"email" => "maizatulfatihah@ymail.com",
+	// 		"course" => "Information Technology"
+	// 	]
+	// ];
 
 	public function getAllMahasiswa()
 	{
-		return $this->mhs; 
+		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+		$this->stmt->execute();
+		return $this->stmt->fetchAll(PDO::FETCH_ASSOC); 
 	}
 }
